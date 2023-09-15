d5a7f99564a4cdcfdfef7c7358d2bdfbfee9f2a4
sungjoon92 <wsjtjdwns92@naver.com>
2023-09-15 16:32:58 +0900

    버퍼리더(한줄씩 끊어읽기), 행사이 밑줄긋기

--- oop0915/Test02_File/Test04_input.java
+++ oop0915/Test02_File/Test04_input.java
@@ -13,43 +13,51 @@ public class Test04_input {
 		BufferedReader br = null;
 
 		try {
-			//1) 파일 가져오기(읽기모드)
+			// 1) 파일 가져오기(읽기모드)
 			fr = new FileReader(filename);
-			
-			//2( 엔터단위로 끊어서 읽기 위해 BufferedReader에 옮겨 담기
+
+			// 2( 엔터단위로 끊어서 읽기 위해 BufferedReader에 옮겨 담기
 			br = new BufferedReader(fr);
-			
-			int num=0;//행번호
-			
-			while(true) {
-				String line = br.readLine();//3) 엔터(\n)를 기준으로 한줄씩 가져오기
-				if(line == null) { //파일의 끝인지?(EOF)
+
+			int num = 0;// 행번호
+
+			while (true) {
+				String line = br.readLine();// 3) 엔터(\n)를 기준으로 한줄씩 가져오기
+				if (line == null) { // 파일의 끝인지?(EOF)
 					break;
-				}//if end
-				//20행마다 밑줄 긋기
-				if(num%20==0) {
+				} // if end
+					// 20행마다 밑줄 긋기
+				if (num % 20 == 0) {
 					System.out.println("----------------------------------------");
-				}//if end
-			System.out.printf("%d %s\n", ++num, line);
-			}//while end
-			
-			//20행마다 밑줄 긋기
-			if(num%20==0) {
+				} // if end
+				System.out.printf("%d %s\n", ++num, line);
+			} // while end
+
+			// 20행마다 밑줄 긋기
+			if (num % 20 == 0) {
 				System.out.println("----------------------------------------");
-			}//if end
-			
+			} // if end
+
 		} catch (Exception e) {
 			System.out.println("파일 읽기 실패:" + e);
 		} finally {
-			//자원 반납 순서 주의
+
+			// 자원 반납 순서 주의
 			try {
-				if(br!=null) {br.close();}
-			} catch (Exception e) {}
-				// TODO: handle exception
+				if (br != null) {
+					br.close();
+				} // if end
+			} catch (Exception e) {
+			} // try end
+
 			try {
-				if(br!=null) {br.close();}
-			} catch (Exception e) {}
-		}//finally end
-		
+				if (br != null) {
+					br.close();
+				} // if end
+			} catch (Exception e) {
+			} // try end
+
+		} //if ~ finally end
+
 	}// main end
 }// class end
